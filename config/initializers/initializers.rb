79267968af8bb5ac9037869902f101dc516e5281
Benny Degezelle <benny@gorilla-webdesign.be>
2010-08-10 23:19:29 +0200

    making I18n compatible, adding nl-BE translation

--- config/initializers/initializers.rb
+++ config/initializers/initializers.rb
@@ -1,8 +1,8 @@
 INQUIRY_TYPES = [
-  'General Information',
-  'Order Information', 
-  'Feedback', 
-  'Feature Request', 
-  'Bug Report', 
-  'Other'
+  I18n.t('contact_us.kinds_of_questions.general_info'),
+  I18n.t('contact_us.kinds_of_questions.order_info'), 
+  I18n.t('contact_us.kinds_of_questions.feedback'), 
+  I18n.t('contact_us.kinds_of_questions.feature_request'), 
+  I18n.t('contact_us.kinds_of_questions.bug_report'), 
+  I18n.t('contact_us.kinds_of_questions.other')
   ]
