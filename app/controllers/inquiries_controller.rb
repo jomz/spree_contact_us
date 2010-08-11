3ffb3457405d12a5873f4f7bf2b04596fd7637ec
Benny Degezelle <benny@gorilla-webdesign.be>
2010-08-11 14:55:21 +0200

    after new inquiry, redirect to products_url (home)

--- app/controllers/inquiries_controller.rb
+++ app/controllers/inquiries_controller.rb
@@ -3,15 +3,18 @@ class InquiriesController < ApplicationController
   
   resource_controller
   
-  create.flash "Got it! We'll be back in touch shortly."
+  create.flash I18n.t("contact_us.thanks_for_getting_in_touch")
+  
+  create do
+    success.wants.html {redirect_to products_url}
+		failure.wants.html {redirect_to(:back)}
+  end
   
   create.after do
     if params[:mailing_list] 
       # make call to mail engine API
     end
-    
   end
-  
   
   def index
     redirect_to(new_inquiry_url) unless params[:inquiry]
